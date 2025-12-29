003305c859abc7cb01fb261aed9208d6e34dbf37
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-29 06:07:33 +0100

    feat: add contract constants

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,4 @@
+
+export const CONTRACT_ADDRESS = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'; // Devnet/Testnet deployer
+export const CONTRACT_NAME = 'LeBaguette';
+export const MINT_PRICE = 50000000; // 0.5 STX
